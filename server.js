8005e3e1e5575a6bc716f9c394b766606329c046
DevanshUpadhyay <devanshupadhyay2611@gmail.com>
2023-08-19 14:57:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is working on port : ${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.send("I ma solar server");
+});
