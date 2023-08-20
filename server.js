fe16d6bf16ea12e9e376157e30847277d1a32bba
devanshupadhyayfreelance <devanshupadhyayfreelance@gmail.com>
2023-08-20 14:52:51 +0530

    20 August 2

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is working on port : ${PORT}`);
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.listen(8000, () => {
+  console.log(`Server is working on port : 8000`);
 });
 app.get("/", (req, res) => {
-  res.send("I am solar server.");
+  res.send(
+    `<h1>Server is Working Fine. Please Click <a href=solar-power>here</a> to visit the Frontend </h1>`
+  );
 });
